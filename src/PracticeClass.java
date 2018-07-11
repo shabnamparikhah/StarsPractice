5e8ce9b97ad03a816d70c9c21c1bd1a644616cc1
shabnam <sparikhah@gmail.com>
2018-07-11 16:14:55 -0400

    Stars

---
+++ src/PracticeClass.java
@@ -0,0 +1,17 @@
+public class PracticeClass {
+
+    public static void main(String[] args)
+    {
+       // System.out.println("****** \n * \t * \n * \t * \n * \t * \n * \t * \n ******");
+
+        System.out.println(" ***** \n * \t * \n * \t * \n * \t * \n * \t * \n *****");
+
+
+
+        System.out.print("   *** \n  *   * \n *     *\n *     * \n  *   * \n   *** \n");
+
+
+        System.out.print("   * \n  * * \n *   *\n  * *\n   *");
+
+    }
+}
